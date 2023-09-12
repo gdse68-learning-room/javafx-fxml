afbdcbe6db55a7d0980abfa1637019daee7f7ad1
danu <danujagreru@gmail.com>
2023-09-12 15:34:43 +0530

    initial commit

---
+++ src/main/java/lk/ijse/javafx/Main.java
@@ -0,0 +1,9 @@
+package lk.ijse.javafx;
+
+/*
+    @author DanujaV
+    @created 9/12/23 - 3:33 PM   
+*/
+
+public class Main {
+}
---
+++ src/main/java/lk/ijse/javafx/controller/MainFormController.java
@@ -0,0 +1,9 @@
+package lk.ijse.javafx.controller;
+
+/*
+    @author DanujaV
+    @created 9/12/23 - 3:34 PM   
+*/
+
+public class MainFormController {
+}
